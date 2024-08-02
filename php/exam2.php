8230e624c78cb71d1b042852947ed12c14f595f6
aman07hk <aman07hk@gmail.com>
2024-08-02 16:19:08 +0530

    php

---
+++ php/exam2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP:example 1</title>
+    <script>
+       var x=10, y=20;
+       var z=x+y;
+       document.write("sum of "+x+" and "+y+" is : "+z);
+       'document.getElementById("p1").innerHTML="i am "';
+    </script>
+</head>
+<body>
+<?php
+$x=10;
+$y=20;
+$z=$x+$y;
+print( "<br><br>sum of ".$x." and ".$y." is :" .$z);
+?>
+<h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis ab magnam rerum voluptates, aliquid quidem perferendis nisi. Culpa aliquid repellendus eum! Harum pariatur velit excepturi ex rem. Quia, ut voluptates.
+<p id="p1">
+
+</body>
+
+</html>
